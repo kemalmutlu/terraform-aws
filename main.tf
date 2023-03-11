191d08ec05fbe70f4bf4c9c6544dfe1771ebf765
Kemal Mutlu <mutlukemal1717@gmail.com>
2023-03-11 19:41:40 +0300

    added admin policy and iam user policy attachment resource

--- main.tf
+++ main.tf
@@ -4,3 +4,16 @@ resource "aws_iam_user" "admin-user" {
     Description = "Technical Team Leader"
   }
 }
+
+resource "aws_iam_policy" "adminUser" {
+  name = "AdminUsers"
+  policy = file("admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "lucy-admin-access" {
+  user = aws_iam_user.admin-user.name
+
+  policy_arn = aws_iam_policy.adminUser.arn
+
+}
+
