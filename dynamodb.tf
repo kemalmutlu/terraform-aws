403ad098c63e5ca30c462dc3e99d3e05282b649c
Kemal Mutlu <mutlukemal1717@gmail.com>
2023-03-12 15:37:24 +0300

    added dynamodb resource

---
+++ dynamodb.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "cars" {
+  name         = "cars"
+  hash_key     = "VIN"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "VIN"
+    type = "S"
+  }
+}
+
+
+resource "aws_dynamodb_table_item" "car-items" {
+  table_name = aws_dynamodb_table.cars.name
+  hash_key   = aws_dynamodb_table.cars.hash_key
+  item       = <<ITEM
+{
+  "Manufacturer": {"S": "Toyota"},
+  "Make": {"S": "Corollo"},
+  "Year": {"N": "2023"},
+  "VIN": {"S": "4Y1SL6"}
+}
+ITEM
+}
